bebb72d404792b8cb2f59593024c00759ec79966
DESKTOP-7RPRRUL\5kyc1ad <skyclad0x7b7@gmail.com>
2016-11-22 15:32:44 +0900

    execute scripts

--- pacro.py
+++ pacro.py
@@ -4,7 +4,7 @@ import codecs
 import json
 
 from lib.gui.Main import Ui_MainWindow
-
+from lib.pacro_core import PacroExecutor
 from PyQt4 import QtCore, QtGui, QtWebKit
 
 class PacroGui(QtGui.QMainWindow):
@@ -14,12 +14,13 @@ class PacroGui(QtGui.QMainWindow):
 		self.ui.setupUi(self)
 
 		# Set list column
-		self.ui.list_script.setColumnWidth(0, 50)
-		self.ui.list_script.setColumnWidth(1, 100)
-		self.ui.list_script.setColumnWidth(2, 100)
+		self.ui.list_script.setColumnWidth(0, 40)
+		self.ui.list_script.setColumnWidth(1, 80)
+		self.ui.list_script.setColumnWidth(2, 140)
 
 		# Set Signal
 		QtCore.QObject.connect(self.ui.button_open, QtCore.SIGNAL("clicked()"), self.button_open_clicked)
+		QtCore.QObject.connect(self.ui.button_run, QtCore.SIGNAL("clicked()"), self.button_run_clicked)
 
 	
 	def button_open_clicked(self):
@@ -32,6 +33,16 @@ class PacroGui(QtGui.QMainWindow):
 			except:
 				# Invalid Json File
 				self.show_messagebox('Error', 'Invalid Json File!', QtGui.QMessageBox.Warning)
+				return False
+
+			self.update_list_script(self.script)
+
+	def update_list_script(self, script):
+		for s in script:
+			new_script = QtGui.QTreeWidgetItem(self.ui.list_script)
+			new_script.setText(0, str(s['id']))
+			new_script.setText(1, s['op'])
+			new_script.setText(2, self.arg_parse(s['arg']))
 
 	def show_messagebox(self, msg_title, msg_text, msg_icon, msg_buttons = [], msg_detail = None):
 		message = QtGui.QMessageBox(self)
@@ -47,8 +58,20 @@ class PacroGui(QtGui.QMainWindow):
 		message.exec_()
 		return message.clickedButton().text()
 
+	def arg_parse(self, dic):
+		ret = []
+		for i in dic.keys():
+			ret.append("{0}={1}".format(i, dic[i]))
+		return ",\n".join(ret)
+
+	def button_run_clicked(self):
+		executor = PacroExecutor(self.script)
+		executor.execute()
+
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
 	pacro_window = PacroGui()
 	pacro_window.show()
 	sys.exit(app.exec_())
+
+# @ Todo : MultiThreading
